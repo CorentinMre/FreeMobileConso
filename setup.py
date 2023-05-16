c6c42f01f1211b8fa3744a87f2c16b87587666eb
StevSpotted <41762297+StevSpotted@users.noreply.github.com>
2023-05-16 19:44:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='FreeMobileConso',
-    version='0.1.1',    
+    version='0.1.2',    
     description='A python API for get your consommation of your Free mobile account',
     long_description_content_type = "text/markdown",
     long_description=long_description,
